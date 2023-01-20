769443c30d133a20a18970a771720fe5f2450a6f
Sudhanshu raut <sudhanshuraut1@gmail.com>
2023-01-21 03:34:20 +0530

    second attempt

--- src/main/java/com/driver/A.java
+++
@@ -1,7 +0,0 @@
-package com.driver;
-
-public class A {
-    String meth(){
-        return  "Invoking method from class A";
-    }
-}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,9 +1,22 @@
 package com.driver;
 
 public class Main {
+    static class A {
+        public String meth(){
+            return  "Invoking method from class A";
+        }
+    }
+    static class B extends A{
+        @Override
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
     public static void main(String[] args) {
-        B b = new B();
-        b.meth();
-        b.meth();
+         B b = new B();
+         b.meth();
+         B b1 = new B();
+         b1.meth();
     }
 }
